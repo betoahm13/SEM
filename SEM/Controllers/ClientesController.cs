5e633b7e1c8a1d89cc2cd05014f9ea0add2c767b
ealvarado <ealvarado@octanesystems.mx>
2022-11-18 22:34:53 -0800

    se realiza prueba de commit

--- SEM/Controllers/ClientesController.cs
+++ SEM/Controllers/ClientesController.cs
@@ -4,6 +4,7 @@ namespace SEM.Controllers
 {
     public class ClientesController : Controller
     {
+        //beto puedes ver mis cambios?
         public IActionResult Index()
         {
             return View();
